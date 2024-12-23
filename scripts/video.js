95058d27283293b5a14e551470a9d67f21cafede
coder-binary1 <noman.g341@gmail.com>
2024-12-23 18:33:37 +0600

    dynamically get the categotirs data

--- scripts/video.js
+++ scripts/video.js
@@ -1,1 +1,19 @@
-console.log("video script added");
+// console.log("video script added");
+// 1 - Fetch, Load and Show categories on html
+
+//Create loadCategories
+const loadCategories = () => {
+  //fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((err) => console.log(err));
+};
+
+//Create displayCategories
+const displayCategories = (data) => {
+  //add Data inside HTML
+  console.log(data);
+};
+
+loadCategories();
